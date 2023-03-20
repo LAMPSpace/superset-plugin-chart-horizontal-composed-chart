e49af9a373eb1f43b6e936b9e191e3c9ae37ca02
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-03-20 09:51:31 +0700

    Init custom plugin superset

---
+++ lib/SupersetPluginChartHorizontalComposedCustom.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+import { SupersetPluginChartHorizontalComposedCustomProps } from './types';
+/**
+ * ******************* WHAT YOU CAN BUILD HERE *******************
+ *  In essence, a chart is given a few key ingredients to work with:
+ *  * Data: provided via `props.data`
+ *  * A DOM element
+ *  * FormData (your controls!) provided as props by transformProps.ts
+ */
+export default function SupersetPluginChartHorizontalComposedCustom(props: SupersetPluginChartHorizontalComposedCustomProps): JSX.Element;
+//# sourceMappingURL=SupersetPluginChartHorizontalComposedCustom.d.ts.map
